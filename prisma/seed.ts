2049b5c0c23521eb2af7d85b9fbd4a8c6f2498a0
Alexis LEROY <alexcool68@free.fr>
2024-07-21 11:32:26 +0200

    .

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const initialPosts: Prisma.PostCreateInput[] = [
+    {
+        title: 'Post 01',
+        slug: 'slug-01',
+        content: 'content',
+        published: true,
+        author: {
+            connectOrCreate: {
+                where: {
+                    email: 'john@gmail.com'
+                },
+                create: {
+                    email: 'john@gmail.com',
+                    password: 'alexis'
+                }
+            }
+        }
+    }
+];
+
+async function main() {
+    console.log('Start seeding');
+    for (const post of initialPosts) {
+        const newPost = await prisma.post.create({
+            data: post
+        });
+        console.log(`Created post with id: ${newPost.id}`);
+    }
+    console.log('Stop seeding');
+}
+
+main();
